9c85728f673b50c4dd278552f35349f2185e46b8
LeeSungje <35910862+LeeSungje@users.noreply.github.com>
2018-02-19 18:13:52 +0900

    테스트
    코드업 문제 푼 것 올려보기

---
+++ 코드업기초문제1097번/1097번.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int prime(int n)
+
+{
+	int i, num = 0;
+
+	for (i = 1; i <= n; i++) {
+		if (n%i == 0) { num++; }
+	}
+
+	if (num == 2) { return 1; }
+
+	else { return -1; }
+
+ }
+
+
+
+int main() {
+
+	int i,n, m;
+
+	scanf("%d", &n);
+
+	for (i = 2; i < n; i++) {
+		if (prime(i) == 1 && n%i == 0) {
+			m = n / i;
+			if (prime(m) == 1) {
+				printf("%d %d", i, m);
+				break;
+			}
+		}
+		else {
+			continue;
+		}
+	}
+
+	if (i == n || n == 1) {
+		printf("wrong number");
+	}
+
+	system("PAUSE");
+	return 0;
+}
